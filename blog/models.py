1501a441a3c929d4158208359ecc68397a75e1cd
benyamyin <benyamyin.c@gmail.com>
2026-07-07 23:34:49 +0330

    I write models for user profiles and posts

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+# یک کلاس برای هر مقاله که حاوی متن  عنوانه نویسنده تاریخ ایجاد
+#وتاربیخ آخرین ویرایش است
+class Maqale(models.Model):
+    title = models.CharField(max_length=255)
+    #title
+    matn = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    #عنوان نویسنده بیان می شود نویسندممبر است ولی هر ممبری نویسنده نیست!
+    #هر نویسنهده چنیدن مقاله دارد ولی هر مقاله یک نویسنده دارد
