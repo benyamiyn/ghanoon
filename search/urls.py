4cf032d51b901b96fd74e98c3f965bed532327d0
benyamyin <benyamyin.c@gmail.com>
2026-07-13 20:45:45 +0330

    i create urls.py for search app

--- search/urls.py
+++ search/urls.py
@@ -1,8 +1,32 @@
-from django.urls import path
-from . import views
+from django.shortcuts import render
+from django.db.models import Q
 
-app_name = "search"
+from blog.models import Maqale
+from .forms import SearchForm 
 
-urlpatterns = [
-    path("",views.search,name="search"),
-]
+def search(requests):
+    form = SearchForm(requests.GET)
+    
+    articles = Maqale.objects.none()
+    
+    if form.is_valid():
+        
+        q = form.cleaned_data["q"]
+        
+        if q:
+            
+            
+            articles = Maqale.objects.filter(
+            Q(title__icontains=q) |
+            Q(matn__icontains=q) |
+            Q(author__username__icontains=q) |
+            Q(category__title__icontains=q)
+        ).distinct()
+    context = {
+        "form":form,
+        "articles":articles,
+        "query": q if form.is_valid() else "",
+        
+    }
+    return render(requests,"search/results.html",context)
+            
