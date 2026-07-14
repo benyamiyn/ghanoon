22198fa5d6b17b3cf08763b5883d3e8fa12622b7
benyamyin <benyamyin.c@gmail.com>
2026-07-14 10:31:38 +0330

    i edit comment model

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
+from django.urls import reverse 
 
 # Create your models here.
 # یک کلاس برای هر مقاله که حاوی متن  عنوانه نویسنده تاریخ ایجاد
@@ -67,15 +68,39 @@ class Comment(models.Model):
         on_delete= models.CASCADE,
         related_name = "comments"
     )
+    parent = models.ForeignKey(
+        "self",
+        on_delete = models.CASCADE,
+        null = True,
+        blank = True,
+        related_name = "children",
+   )
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True,)
+    is_edited = models.BooleanField(default=False)
+    class Meta:
+        ordering =[
+            "created_at",
+        ]
 #مدلی برای کامنت ها که یک فرد می تواند چنذد کامنت بذارد و هر مقاله می تواند چند کامنت داشته باشد 
     def __str__(self):
         
         return f"{self.author.username} - {self.maqale.title}"
 
-
+    @property
+    def is_parent(self):
+        
+        return self.parent is None
+    
+    def get_absolute_url(self):
+        return reverse(
+            "blog:detail",
+            kwargs={
+                "slug": self.maqale.slug,
+            },
+        )
     
 class Like(models.Model):
     
