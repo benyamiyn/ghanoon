4d46345bf63db6c3d68762ea2378fd346a3ad37a
benyamyin <benyamyin.c@gmail.com>
2026-07-15 13:48:12 +0330

    i write views and urls for blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Maqale
 
 # Create your views here.
+
+def maqale_detail(request,slug):
+    maqale = get_object_or_404(
+        Maqale,
+        slug = slug
+    )
+    context = {
+        "maqale" : maqale,
+    }
+    return render(
+        request,
+        "blog/post_detail.html",
+        context
+    )
+    
+
