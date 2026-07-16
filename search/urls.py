f0816d344ac89e79e96acb9aca3d1e5df5f83b20
benyamyin <benyamyin.c@gmail.com>
2026-07-16 14:19:30 +0330

    i fix search app problems

--- search/urls.py
+++ search/urls.py
@@ -1,21 +1,11 @@
 from django.urls import path
+
 from . import views
 
 app_name = "search"
 
 urlpatterns = [
     path("", views.search, name="search"),
-    path(
-        "suggest/",
-        views.suggest,
-        name="suggest"
-    ),
-    path(
-        "live/",
-        views.live_search,
-        name="live"
-    )
+    path("suggest/", views.suggest, name="suggest"),
+    path("live/", views.live_search, name="live"),
 ]
-
-# تعریف کردن مسیری برای اپ
-#مسیری برای فیچر های جست و جو پیشنهادی و جست و جو زنده
