22a1d22ae4836c9b9683a0ade1093e01c006f92c
benyamyin <benyamyin.c@gmail.com>
2026-07-18 10:06:36 +0330

    i fix blog/urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,19 +4,8 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path(
-        "<slug:slug>/",
-        views.maqale_detail,
-        name="detail",
-    ),
-    path(
-        "<slug:slug>/comment/add/",
-        views.add_comment,
-        name="add_comment",
-    ),
-    path(
-        "<slug:slug>/like/",
-        views.toggle_like,
-        name="toggle_like",
-    ),
+    path("", views.home, name="home"),
+    path("<slug:slug>/", views.maqale_detail, name="detail"),
+    path("<slug:slug>/comment/add/", views.add_comment, name="add_comment"),
+    path("<slug:slug>/like/", views.toggle_like, name="toggle_like"),
 ]
